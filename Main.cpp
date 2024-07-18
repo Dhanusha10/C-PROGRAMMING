29d830fd95189c9688d9521ec92555789704dca3
Dhanusha10 <dhanushathatchayini.m@rapidcare.ai>
2024-07-18 11:49:49 +0530

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+using namespace std;
+int main(){
+
+  int age = 23;
+  string Name = "John";
+  string Address = " India";
+
+  cout<<Name << " is " << age<< " and He is from "<< Address<< endl;
+  return 0; 
+}
